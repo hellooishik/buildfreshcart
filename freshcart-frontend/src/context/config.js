542597b26f4e4868081d34bdd37f73038cd37025
Oishik dutta <129034058+hellooishik@users.noreply.github.com>
2025-03-21 18:03:43 +0530

    Delete freshcart-frontend directory

--- freshcart-frontend/src/context/config.js
+++
@@ -1,9 +0,0 @@
-const getBaseURL = () => {
-    const isLocalhost = window.location.hostname === 'localhost';
-    return isLocalhost
-      ? 'http://localhost:4000'
-      : 'https://buildfreshcart-3.onrender.com';
-  };
-  
-  export const API_URL = getBaseURL();
-  
--- freshcart-frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://buildfreshcart-3.onrender.com"; // Replace with your backend URL
-
-export const api = axios.create({
-  baseURL: API_URL,
-  headers: { "Content-Type": "application/json" },
-});
